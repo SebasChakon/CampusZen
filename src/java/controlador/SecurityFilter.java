9efef0128d4df26675940e669066c796706d5e64
Sebastian Chacón Mantilla <sebastianchmantilla@gmail.com>
2026-05-03 14:44:40 -0500

    Protocolo https

---
+++ src/java/controlador/SecurityFilter.java
@@ -0,0 +1,49 @@
+package controlador;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import java.io.IOException;
+
+public class SecurityFilter implements Filter {
+
+    private static final boolean USE_HTTPS = true;
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response,
+                         FilterChain chain)
+            throws IOException, ServletException {
+
+        HttpServletRequest req = (HttpServletRequest) request;
+        HttpServletResponse res = (HttpServletResponse) response;
+
+        if (USE_HTTPS && !req.isSecure()) {
+
+            String url = "https://" + req.getServerName()
+                    + ":8443"
+                    + req.getRequestURI();
+
+            if (req.getQueryString() != null) {
+                url += "?" + req.getQueryString();
+            }
+
+            res.sendRedirect(url);
+            return;
+        }
+
+        if (!USE_HTTPS && req.isSecure()) {
+
+            String url = "http://" + req.getServerName()
+                    + ":8080"
+                    + req.getRequestURI();
+
+            if (req.getQueryString() != null) {
+                url += "?" + req.getQueryString();
+            }
+
+            res.sendRedirect(url);
+            return;
+        }
+
+        chain.doFilter(request, response);
+    }
+}
